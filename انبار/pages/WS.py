fcf40089e7d3a56ec6e6a985907b05c0a6ebb4eb
morteza rasooli <144559589+MrShotgun1182@users.noreply.github.com>
2024-12-19 13:48:36 +0330

    Delete انبار directory

--- انبار/pages/WS.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from Engin import make_WS_DF
-
-st.set_page_config(page_title="WS")
-
-st.write("# :موجودی انبار به شرح زیر میباشد ")
-
-st.dataframe(make_WS_DF(), use_container_width=True, selection_mode="multi-row")
-
