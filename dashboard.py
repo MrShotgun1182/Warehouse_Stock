66d14f70ce79f2d920542f5b22a31b222df2283b
morteza rasooli <motteza1182@gmail.com>
2024-12-27 11:43:22 +0330

    make dashboard

---
+++ dashboard.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+def bar():
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+    
+    login_page = st.Page("accont\login_page.py", title="Log in", icon=":material/login:")
+    logout_page = st.Page("accont\logout_page.py", title="Log out", icon=":material/logout:")
+
+    warehouse = st.Page("انبار\موجودی انبار.py", title="موجودی انبار")
+    i_o = st.Page("انبار\ورودی و خروجی.py", title="ورودی و خروجی")
+    products = st.Page("انبار\محصولات.py", title="محصولات")
+    
+    lend = st.Page("امانات\امانت.py", title="امانت")
+    persons = st.Page("امانات\مخاطبین.py", title="مخاطبین")
+    
+    finance = st.Page("حسابداری\حسابداری.py", title="حسابداری")
+    
+    if st.session_state.logged_in:
+        page = st.navigation(
+                {
+                    "حساب کاربری": [logout_page],
+                    "انبار": [warehouse, i_o, products],
+                    "امانات": [lend, persons],
+                    "حسابداری": [finance]
+                },
+                position="sidebar"
+            )
+    else:
+        page = st.navigation(
+                {
+                    "حسباکاربری": [login_page]
+                }
+            )
+
+    page.run()
+    
+def main():
+    bar()
+
+main()
