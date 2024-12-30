9c41eefbeead915fcbfa3090f66d9c61ca900aa0
MR surface <motteza1182@gmail.com>
2024-12-30 11:06:46 +0330

    make login page

--- accont/login_page.py
+++ accont/login_page.py
@@ -1,8 +1,27 @@
 import streamlit as st
+from Engine import back_login_page
 
 def header():
     st.set_page_config(page_title="login page")
 
+def login_form():
+    with st.form("login_form"):
+        user_name = st.text_input(label="نام کاربری خود را وارد کنید")
+        password = st.text_input(label="رمز عبور خود را وارد کنید")
+        submit = st.form_submit_button(label="ورود", use_container_width=True)
+
+    if submit:
+        input_dic = {
+            "user_name": user_name,
+            "password": password
+        }
+        account_status = back.test_account(input_dic)
+        if account_status == 200:
+            st.session_state.level_account = back.__level_account__
+            st.session_state.logged_in = True
+            st.rerun()
+        else:
+            st.write(back.__account_status__)
 
 def login():
     st.write("its login page")
@@ -11,6 +30,8 @@ def login():
         st.rerun()
 
 def main():
-    login()
+    header()
+    login_form()
 
+back = back_login_page()
 main()
